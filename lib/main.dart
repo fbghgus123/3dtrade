1249c3fddcc85a2e0841a4d40052de82d7daee2a
fbghgus123 <74997185+fbghgus123@users.noreply.github.com>
2022-08-18 22:46:22 +0900

    Add firebase core

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:kakao_flutter_sdk_auth/kakao_flutter_sdk_auth.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'app/constants/app.strings.dart';
@@ -8,6 +10,9 @@ import 'app.routing.dart';
 void main() async {
   await dotenv.load(fileName: ".env");
   KakaoSdk.init(nativeAppKey: '${dotenv.env["KAKAO_LOGIN_API_KEY"]}');
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     GetMaterialApp(
       title: AppStrings.appTitle,
